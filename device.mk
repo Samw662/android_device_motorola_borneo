75b58280281226ee641103cac8f75e00cc17993f
mathieuchartier <mathieu.a.chartier@gmail.com>
2024-08-09 16:19:29 +0200

    borneo: Speed profile services
    * Speed profile services and wifi-service to reduce RAM and storage

--- device.mk
+++ device.mk
@@ -43,3 +43,6 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/borneo/borneo-vendor.mk)
+
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
